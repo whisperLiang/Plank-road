ef9f6e374830e73d5230e768db4f690d6cbbd8cc
whisperLiang <1851438730@qq.com>
2026-03-01 15:36:46 +0800

    Re-export universal model splitting and Dynamic Activation Sparsity APIs in model_management package

---
+++ model_management/__init__.py
@@ -0,0 +1,40 @@
+# model_management package
+
+# Re-export universal model splitting API when torchlens is available
+try:
+    from model_management.universal_model_split import (  # noqa: F401
+        UniversalModelSplitter,
+        extract_split_features,
+        save_split_feature_cache,
+        load_split_feature_cache,
+        universal_split_retrain,
+        LayerInfo,
+        LayerProfile,
+        SplitPointSelector,
+    )
+except ImportError:
+    pass
+
+# Re-export Dynamic Activation Sparsity (SURGEON-style) API
+try:
+    from model_management.activation_sparsity import (  # noqa: F401
+        DASTrainer,
+        apply_das_to_model,
+        apply_das_to_tail,
+        AutoFreezeConv2d,
+        DASBatchNorm2d,
+        AutoFreezeFC,
+        ActivationClipper,
+        compute_tgi,
+    )
+except ImportError:
+    pass
+
+# Re-export Model Zoo (unified detection model factory)
+from model_management.model_zoo import (  # noqa: F401
+    build_detection_model,
+    list_available_models,
+    is_wrapper_model,
+    model_has_roi_heads,
+    get_model_family,
+)
